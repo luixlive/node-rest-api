d27aba05eb7222c2d9caaea2ac8f0f95398abc81
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-08 20:13:36 -0500

    Add post route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', ({ body: { text }}, res) => {
+  const todo = new Todo({ text });
+
+  todo.save().then((doc) => {
+    res.status(200).send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
