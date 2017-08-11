ec1151d31df21a4e2b27f7335118afb318ca8769
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-10 19:48:27 -0500

    Generate token for each user

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,59 @@
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    require: true,
+    required: true,
     minlength: 1,
     trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+    },
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
 });
+
+UserSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({
+    _id: user._id.toHexString(),
+    access,
+  }, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
