1950eba7e9752dc4bc6002b15f16eb3102e7f975
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-12 17:28:33 -0500

    Add delete users me token route

--- server/models/user.js
+++ server/models/user.js
@@ -92,6 +92,16 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token},
+    },
+  });
+};
+
 UserSchema.pre('save', function(next) {
   const user = this;
 
