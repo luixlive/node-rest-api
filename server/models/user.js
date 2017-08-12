91572d0b520d9e84a16ee45d1932ac2b2c6fdecd
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-12 15:03:43 -0500

    Add post user login rout

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,26 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
