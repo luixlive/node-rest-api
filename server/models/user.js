db86ab14c7736005361e4170469d006f0011316a
Luis Alfonso <luis.chavez@wizeline.com>
2017-08-12 19:12:46 -0500

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -80,7 +80,7 @@ UserSchema.statics.findByToken = function(token) {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
